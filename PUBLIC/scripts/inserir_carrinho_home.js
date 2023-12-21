d6419ec237bb092436a67294d3d4e75a1669566c
ZacksFy <86888690+ZacksFy@users.noreply.github.com>
2023-12-21 06:06:30 -0300

    Add files via upload

---
+++ PUBLIC/scripts/inserir_carrinho_home.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    for (var i = 1; i <= 100; i++){
+        let id = '#product-cart-click' + i;
+        let Nome = $(id).text();
+
+        $(id).click(function(){
+            var Data = {nome: Nome};
+
+            $.post("home.php", Data, function(response) {
+            }, 'json');
+        })
+    }  
+})
